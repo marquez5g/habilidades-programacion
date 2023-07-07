b7839509cd8a0f7c92678588086d8c37d2fb16a5
Gabriel Marquez <marquez122@outlook.com>
2023-07-07 13:25:15 -0500

    employee repository

---
+++ src/main/java/com/empresa/empleados/repositories/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.empresa.empleados.repositories;
+
+import com.empresa.empleados.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
